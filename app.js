14cffc741a491379c6810c3ef17b11ee7b3ab074
Franco <rotchen1@gmail.com>
2023-02-07 23:56:53 -0300

    Modificando app.js para subirlo a render

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const publicPath = path.join(__dirname, "public");
 app.use(express.static(publicPath));
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
