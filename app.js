5781bb4276f8dadde1638b2f738a01589d5fe1ee
Franco <rotchen1@gmail.com>
2023-02-07 22:31:00 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const publicPath = path.join(__dirname, "public");
+
+app.use(express.static(publicPath));
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/views/register.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+
+app.get("/views/login.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
